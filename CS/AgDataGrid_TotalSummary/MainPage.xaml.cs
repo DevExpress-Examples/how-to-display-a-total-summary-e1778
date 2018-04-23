d92edeb35b565560b4872e8b801ac34b3400d8d0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/AgDataGrid_TotalSummary/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Controls;
+
+namespace AgDataGrid_TotalSummary {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid.DataSource = ProductList.GetData();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to Display a Total Summary
+
+
+<p>This example shows how to display totals. To do this, set the grid's ShowTotals property to 'True', create required AgDataGridSummaryItem objects and add them to the grid's TotalSummary collection.</p>
+
+<br/>
+
+
